ac4e08e58f2e9c3109d1ad559b776699c7d36bff
Priyansh Tripathi <71260531+PriyanshTri@users.noreply.github.com>
2024-09-05 09:27:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find all factorial numbers less than or equal to n/find-all-factorial-numbers-less-than-or-equal-to-n.java
+++ Difficulty: Easy/Find all factorial numbers less than or equal to n/find-all-factorial-numbers-less-than-or-equal-to-n.java
@@ -26,16 +26,23 @@ class GFG {
 // User function Template for Java
 class Solution {
     static ArrayList<Long> factorialNumbers(long n) {
+        //never use static variables
         ArrayList<Long> output = new ArrayList<>(Arrays.asList(1L));
+        // we can pass output array and update it according to our usecase
         getFacts(n, 1, output);
         return output;
     }
 
     static void getFacts(long n, long i, ArrayList<Long> output) {
+        // base case needs to be specific and more accurate
         if (output.isEmpty() || output.get(output.size() - 1) > n) return;
+        // if recieving end is larger variable type then addition or subtraction, then autobox will work
         long value = (i + 1) * output.get((int)(i-1));
+        
         if (value <= n) {
+            // add the value in output array(array recieved from outside)
             output.add(value);
+            // recursive call to go to base case (to avoid TLE and give proper output)
             getFacts(n, i + 1, output);
         }
     }
